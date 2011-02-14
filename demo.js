31e8840992176e220a70f93bdd827f6f54b07edd
Ashleigh Baumgardner <labaumgardner@gmail.com>
2011-02-13 16:10:40 -0800

    Changed some names

--- demo.js
+++ demo.js
@@ -7,7 +7,13 @@ $(function () {
   if( localStorage.getItem('code')) {
     $("#code").val(localStorage.getItem("code"));
   } else {
-    $("#code").val('paper.circle(420, 340, 60).animate({fill: "blue", stroke: "#000", "stroke-width": 40, "stroke-opacity": 0.5}, 2000); ');
+    $("#code").val('var p = paper.circle(420, 240, 60).animate({fill: "blue", stroke: "#000", "stroke-width": 40, "stroke-opacity": 0.5}, 2000);\n'+ 
+'var st = paper.set(); \n'+
+'st.push( \n'+
+'  paper.rect(200, 400, 50, 50, 10),\n '+
+'  paper.circle(70, 400, 50) \n'+
+'); \n'+
+'st.animate({fill: "green", stroke: "#000", "stroke-width": 40, "stroke-opacity": 0.5}, 1000);');
   }
 
   setCanvas();
@@ -22,7 +28,7 @@ function setCanvas() {
   paper.clear();
   paper.rect(0, 0, 640, 480, 10).attr({fill: "#fff", stroke: "none"});
   try {
-    (new Function("paper", "window", "document", $("#code").val())).call(paper, paper);
+    (new Function("paper", "window", "document", $("#code").val() ) ).call(paper, paper);
   } catch (e) {
     alert(e.message || e);
   }
